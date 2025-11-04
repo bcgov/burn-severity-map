290e1dcc935ac2b47e3cfe3ca83d377a4a10d013
earmitag <emma.armitage@gov.bc.ca>
2025-11-04 11:33:56 -0800

    update redirect auth url

--- frontend/src/auth/authConfig.ts
+++ frontend/src/auth/authConfig.ts
@@ -4,9 +4,6 @@ import { WebStorageStateStore, UserManagerSettings } from 'oidc-client-ts';
 const baseUrl = process.env.REACT_APP_BASE_URL || 'http://localhost:8080';
 const callbackUrl = process.env.REACT_APP_AUTH_CALLBACK_URL || `${baseUrl}/callback`;
 
-// const baseUrl = 'https://bs-app-burn-severity-map.apps.silver.devops.gov.bc.ca/';
-// const callbackUrl = `${baseUrl}/callback`
-
 const oidcConfig: UserManagerSettings = {
   authority: 'https://dev.loginproxy.gov.bc.ca/auth/realms/standard',
   client_id: 'burn-severity-6058',
