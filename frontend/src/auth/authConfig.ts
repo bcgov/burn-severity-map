89a0d4a8ff4ab2004ce8f3becbd815a406bb25e5
earmitag <emma.armitage@gov.bc.ca>
2025-11-03 17:16:53 -0800

    test different uri for authentication

--- frontend/src/auth/authConfig.ts
+++ frontend/src/auth/authConfig.ts
@@ -1,13 +1,18 @@
 // src/authConfig.ts
 import { WebStorageStateStore, UserManagerSettings } from 'oidc-client-ts';
 
+// const baseUrl = process.env.BASE_LOGIN_URL || 'http://localhost:8080';
+
+const baseUrl = 'https://bs-app-burn-severity-map.apps.silver.devops.gov.bc.ca/';
+
 const oidcConfig: UserManagerSettings = {
   authority: 'https://dev.loginproxy.gov.bc.ca/auth/realms/standard',
   client_id: 'burn-severity-6058',
-  redirect_uri: 'http://localhost:8080/callback',
+  // redirect_uri: 'http://localhost:8080/callback',
+  redirect_uri: `${baseUrl}/callback`,
   response_type: 'code',
   scope: 'openid profile email',
-  post_logout_redirect_uri: 'http://localhost:8080/',
+  post_logout_redirect_uri: baseUrl,
   userStore: new WebStorageStateStore({ store: window.localStorage }),
 };
 
