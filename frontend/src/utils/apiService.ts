489b0ea49e44c3f4a4d13fcea157ac4d3028520f
wburt <william.burt@gov.bc.ca>
2026-06-25 10:25:22 -0700

    add fetch of fire years with bs

--- frontend/src/utils/apiService.ts
+++ frontend/src/utils/apiService.ts
@@ -234,6 +234,26 @@ export const getFireNumbers = async (year:string) => {
   return response.json();
 };
 
+/**
+ * Fetch the list years for with bs results
+ */
+export const getFireYears = async () => {
+  await ensureDataReady();
+
+  const response = await authedFetch(`/years`);
+  if (!response.ok) {
+    // handle non-2xx responses here.
+    const errorData = await response.json().catch(() => ({ detail: 'An unknown error occurred' }));
+    throw new Error(errorData.detail || `HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  
+  // Extract the array, convert numbers to strings
+  if (data && Array.isArray(data.fire_years)) {
+    return data.fire_years.map((y: number | string) => String(y));
+  }
+};
+
 export const syncFireResults = async (year: string,fire_number: string) => {
   await ensureDataReady();
   const response = await authedFetch(`/sync-burn-severity/${year}/${fire_number}`);
