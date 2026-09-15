8ba6b71cf887ace5f40ee105a50760de31f06ce9
GraydonShevchenko <Graydon.Shevchenko@gov.bc.ca>
2026-09-15 15:14:31 -0700

    Add in water masking as well as pulling out basic cloud cover from imagery

--- analysis/barc_analysis.py
+++ analysis/barc_analysis.py
@@ -170,6 +170,7 @@ class InterimBurnSeverity:
         self.__current_points ='WHSE_LAND_AND_NATURAL_RESOURCE.PROT_CURRENT_FIRE_PNTS_SP'
         self.__historic_perimeters = 'WHSE_LAND_AND_NATURAL_RESOURCE.PROT_HISTORICAL_FIRE_POLYS_SP'
         self.__historic_points = 'WHSE_LAND_AND_NATURAL_RESOURCE.PROT_HISTORICAL_INCIDENTS_SP'
+        self.__fwa_lakes = 'WHSE_BASEMAPPING.FWA_LAKES_POLY'
         
 
         self.dict_pre_fire = defaultdict(ImageMetadata)
@@ -185,6 +186,7 @@ class InterimBurnSeverity:
 
         self.gdf_fires = None
         self.fire_boundary = None
+        self.gdf_lakes = None
 
     def __del__(self) -> None:
         pass
@@ -243,7 +245,13 @@ class InterimBurnSeverity:
         
 
         # self.lst_delete_datasets.extend([self.fc_fire_point, self.fc_fire_perimeter])
-
+        self.logger.info('Gathering intersecting lakes')
+        fire_bounds = self.gdf_fires.total_bounds
+        fire_bounds = f'{fire_bounds[0]},{fire_bounds[1]},{fire_bounds[2]},{fire_bounds[3]},urn:ogc:def:crs:EPSG::3005'
+        self.logger.info(fire_bounds)
+        lakes = WFS.get_data(dataset=self.__fwa_lakes, bbox=fire_bounds)
+        self.gdf_lakes = gpd.GeoDataFrame.from_features(features=lakes, crs=3005)
+        self.logger.info(f'Found {self.gdf_lakes.shape[0]} intersecting lakes')
 
         for i, row in self.gdf_fires.iterrows():
             if not self.start_date:
@@ -343,7 +351,11 @@ class InterimBurnSeverity:
                 output_post = f'{self.fire_year}-{fire_number}_post_nbr.tif'
                 output_pre_rgb = f'{self.fire_year}-{fire_number}_pre_rgb.tif'
                 output_post_rgb = f'{self.fire_year}-{fire_number}_post_rgb.tif'
+                output_pre_cloud = f'{self.fire_year}-{fire_number}_pre_cloud.tif'
+                output_post_cloud = f'{self.fire_year}-{fire_number}_post_cloud.tif'
+                output_water = f'{self.fire_year}-{fire_number}_water.tif'
                 output_dnbr = f'{self.fire_year}-{fire_number}_dnbr.tif'
+                output_dnbr_mask = f'{self.fire_year}-{fire_number}_dnbr_masked.tif'
                 output_scaled = f'{self.fire_year}-{fire_number}_scaled_dnbr.tif'
                 output_barc = f'{self.fire_year}-{fire_number}_{pre_fire_date}_{post_fire_date}_{self.sensor}_barc.tif'
                 output_filtered = f'{self.fire_year}-{fire_number}_{pre_fire_date}_{post_fire_date}_{self.sensor}_barc_filtered.tif'
@@ -352,21 +364,32 @@ class InterimBurnSeverity:
                 output_post_nbr_path = os.path.join(self.output_folder, output_post) if self.use_folder else None
                 output_pre_rgb_path = os.path.join(self.export_folder, output_pre_rgb) if self.use_folder else None
                 output_post_rgb_path = os.path.join(self.export_folder, output_post_rgb) if self.use_folder else None
+                output_pre_cloud_path = os.path.join(self.output_folder, output_pre_cloud) if self.use_folder else None
+                output_post_cloud_path = os.path.join(self.output_folder, output_post_cloud) if self.use_folder else None
+                output_water_path = os.path.join(self.output_folder, output_water) if self.use_folder else None
                 output_dnbr_path = os.path.join(self.output_folder, output_dnbr) if self.use_folder else None
+                output_dnbr_mask_path = os.path.join(self.output_folder, output_dnbr_mask) if self.use_folder else None
                 output_scaled_dnbr_path = os.path.join(self.output_folder, output_scaled) if self.use_folder else None
                 output_barc_path = os.path.join(self.output_folder, output_barc) if self.use_folder else None
                 output_filtered_path = os.path.join(self.export_folder, output_filtered) if self.use_folder else None
+
                 os_pre_nbr_path = f'{self.os_output_folder}/{output_pre}' if self.use_storage else None
                 os_post_nbr_path = f'{self.os_output_folder}/{output_post}' if self.use_storage else None
                 os_pre_rgb_path = f'{self.os_export_folder}/{output_pre_rgb}' if self.use_storage else None
                 os_post_rgb_path = f'{self.os_export_folder}/{output_post_rgb}' if self.use_storage else None
+                os_pre_cloud_path = f'{self.os_output_folder}/{output_pre_cloud}' if self.use_storage else None
+                os_post_cloud_path = f'{self.os_output_folder}/{output_post_cloud}' if self.use_storage else None
+                os_water_path = f'{self.os_output_folder}/{output_water}' if self.use_storage else None
                 os_dnbr_path = f'{self.os_output_folder}/{output_dnbr}' if self.use_storage else None
+                os_dnbr_mask_path = f'{self.os_output_folder}/{output_dnbr_mask}' if self.use_storage else None
                 os_scaled_dnbr_path = f'{self.os_output_folder}/{output_scaled}' if self.use_storage else None
                 os_barc_path = f'{self.os_output_folder}/{output_barc}' if self.use_storage else None
                 os_filtered_path = f'{self.os_export_folder}/{output_filtered}' if self.use_storage else None
 
+                
+
                 self.logger.info(f'Creating PRE-FIRE RGB')
-                pre_rgb, pre_meta, pre_transform = stac.create_rgb_mosaic(pre_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs, run_type='pre')
+                pre_rgb, pre_meta, pre_transform = stac.create_rgb_mosaic(pre_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs)
                 if pre_rgb is None:
                     self.logger.error('Failed to create pre-fire RGB.')
                     return None
@@ -379,7 +402,7 @@ class InterimBurnSeverity:
                 gc.collect()
                 
                 self.logger.info(f'Creating POST-FIRE RGB')
-                post_rgb, post_meta, post_transform = stac.create_rgb_mosaic(post_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs, run_type='post')
+                post_rgb, post_meta, post_transform = stac.create_rgb_mosaic(post_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs)
                 if post_rgb is None:
                     self.logger.error('Failed to create post-fire RGB.')
                     return None
@@ -392,7 +415,7 @@ class InterimBurnSeverity:
                 gc.collect()
 
                 self.logger.info(f'Calculating PRE-FIRE NBR')
-                pre_nbr, pre_meta, pre_transform = stac.create_nbr_mosaic(pre_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs, run_type='pre')
+                pre_nbr, pre_cloud, pre_meta, pre_transform = stac.create_nbr_mosaic(pre_fire_items, perimeter_gdf, aws_requester_pays=False, target_crs=perimeter_gdf.crs)
                 if pre_nbr is None:
                     self.logger.error('Failed to calculate pre-fire NBR.')
                     return None
@@ -400,7 +423,7 @@ class InterimBurnSeverity:
 
                 self.logger.info('Writing pre-fire nbr to file')
                 self.write_raster(data=pre_nbr, meta=pre_meta, folder_path=output_pre_nbr_path, os_path=os_pre_nbr_path)
-
+                self.write_raster(data=pre_cloud, meta=pre_meta, folder_path=output_pre_cloud_path, os_path=os_pre_cloud_path)
 
                 # 6. Calculate Post-fire NBR, aligning to the pre-fire grid
                 self.logger.info(f'Calculating POST-FIRE NBR')
@@ -408,14 +431,13 @@ class InterimBurnSeverity:
                 target_crs_for_post = pre_meta['crs']
                 target_transform_for_post = pre_transform
 
-                post_nbr, post_meta, _ = stac.create_nbr_mosaic(
+                post_nbr, post_cloud, post_meta, _ = stac.create_nbr_mosaic(
                     post_fire_items, 
                     perimeter_gdf,
                     target_transform=target_transform_for_post,
                     target_crs=target_crs_for_post,
                     target_shape=target_shape_for_post,
-                    aws_requester_pays=False,
-                    run_type='post'
+                    aws_requester_pays=False
                 )
                 if post_nbr is None:
                     self.logger.error('Failed to calculate post-fire NBR.')
@@ -424,12 +446,14 @@ class InterimBurnSeverity:
 
                 self.logger.info('Writing post-fire nbr to file')
                 self.write_raster(data=post_nbr, meta=post_meta, folder_path=output_post_nbr_path, os_path=os_post_nbr_path)
+                self.write_raster(data=post_cloud, meta=post_meta, folder_path=output_post_cloud_path, os_path=os_post_cloud_path)
 
                 # Ensure alignment before dNBR (should be guaranteed by calculate_nbr_for_item logic)
                 if pre_nbr.shape != post_nbr.shape:
                     self.logger.error(f'CRITICAL ERROR: Pre-fire NBR shape {pre_nbr.shape} and Post-fire NBR shape {post_nbr.shape} '
                           'do not match despite alignment efforts. Cannot proceed with dNBR calculation.')
                     return None
+
 
                 # 7. Calculate dNBR
                 self.logger.info('Calculating dNBR (Pre-NBR - Post-NBR)')
@@ -448,9 +472,22 @@ class InterimBurnSeverity:
                     "nodata": np.nan # Ensure nodata is consistent
                 })
 
+
                 self.logger.info('Writing dnbr to file')
                 self.write_raster(data=dnbr, meta=dnbr_meta, folder_path=output_dnbr_path, os_path=os_dnbr_path)
 
+                self.logger.info('Creating water mask')
+                water_mask = stac.rasterize_water(water=self.gdf_lakes, out_height=pre_nbr.shape[1], out_width=pre_nbr.shape[2], out_transform=pre_transform)
+
+                self.logger.info('Writing water mask to file')
+                self.write_raster(data=water_mask, meta=pre_meta, folder_path=output_water_path, os_path=os_water_path)
+
+
+                self.logger.info('Masking out water')
+                dnbr[0, water_mask] = np.nan
+
+                self.logger.info('Writing masked dnbr to file')
+                self.write_raster(data=dnbr, meta=dnbr_meta, folder_path=output_dnbr_mask_path, os_path=os_dnbr_mask_path)
                 
                 # 8. Calculate scaled dNBR
                 self.logger.info('Calculating scaled dNBR ((dNBR * 1000 + 275)/5)')
@@ -472,7 +509,7 @@ class InterimBurnSeverity:
 
                 # Clean up files to free memory
                 self.logger.info('Cleaning up rasters')
-                del pre_nbr, post_nbr, dnbr
+                del pre_nbr, post_nbr, dnbr, water_mask
                 gc.collect()
 
 
@@ -483,13 +520,14 @@ class InterimBurnSeverity:
                 # barc = no_sev + low_sev + med_sev + high_sev
 
                 self.logger.info('Classifying barc')
-                barc = np.zeros(scaled_dnbr.shape, dtype=np.uint8)
+                # barc = np.zeros(scaled_dnbr.shape, dtype=np.uint8)
+                barc = np.full(scaled_dnbr.shape, 255, dtype=np.uint8)
                 barc[scaled_dnbr < 76] = 1 # unburned
                 barc[(scaled_dnbr >= 76) & (scaled_dnbr < 110)] = 2 # low severity
                 barc[(scaled_dnbr >= 110) & (scaled_dnbr < 187)] = 3 # medium severity
                 barc[(scaled_dnbr >= 187)] = 4 # high severity
 
-                barc[np.isnan(scaled_dnbr)] = 0
+                # barc[np.isnan(scaled_dnbr)] = 0
 
                 # clean up scaled dnbr
                 del scaled_dnbr
@@ -500,7 +538,7 @@ class InterimBurnSeverity:
                     "driver": "GTiff",
                     "dtype": "uint8", # dNBR is float
                     "count": 1,
-                    "nodata": 0 # Ensure nodata is consistent
+                    "nodata": 255 # Ensure nodata is consistent
                 })
 
                 # try:
@@ -508,7 +546,10 @@ class InterimBurnSeverity:
                 self.write_raster(data=barc.astype(np.uint8), meta=s_class_meta, folder_path=output_barc_path, os_path=os_barc_path, delete=True)
 
                 self.logger.info('Filtering barc to remove fragments less than 10 m2')
-                barc_filter = sieve(source=barc.astype(np.uint8), size=10)
+                barc_2d = barc[0].astype(np.uint8)
+                valid_mask = (barc_2d != 255)
+                barc_filtered_2d = sieve(source=barc_2d, size=10, mask=valid_mask)
+                barc_filter = np.expand_dims(barc_filtered_2d, axis=0)
 
                 filter_meta = s_class_meta.copy()
                 filter_meta.update(
@@ -540,8 +581,10 @@ class InterimBurnSeverity:
             # barc_name = os.path.basename(barc_tif)
             self.logger.info('Converting to polygon')
 
+            valid_mask = (barc != 255)
+
             results = ({'properties': {'raster_val': v}, 'geometry': s}
-                        for i, (s, v) in enumerate(shapes(barc, mask=None, transform=meta['transform'])))
+                        for i, (s, v) in enumerate(shapes(barc, mask=valid_mask, transform=meta['transform'])))
 
             geoms = list(results)
             gdf = gpd.GeoDataFrame.from_features(geoms, crs=meta['crs'])
--- analysis/util/stac.py
+++ analysis/util/stac.py
@@ -3,6 +3,7 @@ from rasterio.session import AWSSession
 from rasterio.warp import reproject, Resampling, calculate_default_transform
 from rasterio.io import MemoryFile
 from rasterio.transform import array_bounds
+from rasterio.features import rasterize
 from rasterio import mask
 import geopandas as gpd
 import numpy as np
@@ -128,15 +129,16 @@ class STAC:
                             best_item_found = planetary_computer.sign(best_item_found)
                         item_id = best_item_found.id
                         item_date = best_item_found.datetime.date()
-                        cloud_cover = best_item_found.properties.get('eo:cloud_cover', 'N/A')
+                        cloud_cover = best_item_found.properties.get('eo:cloud_cover', 0)
                         self.logger.info(f'    -> Selected tile {item_id} (Date: {item_date}, Cloud: {cloud_cover}%)')
 
                         selected_items.append(best_item_found)
                         searched_item_ids.add(item_id)
 
-                        # Update the uncovered area
-                        item_geom = shape(best_item_found.geometry)
-                        uncovered_geom = uncovered_geom.difference(item_geom)
+                        # Update the uncovered area if cloud coverage is cloud free
+                        if cloud_cover < 2.0:
+                            item_geom = shape(best_item_found.geometry)
+                            uncovered_geom = uncovered_geom.difference(item_geom)
                     else:
                         self.logger.info('    - No more suitable intersecting tiles found in STAC')
                         if not selected_items:
@@ -168,46 +170,58 @@ class STAC:
                           target_transform=None, 
                           target_crs=None, 
                           target_shape=None,
-                          aws_requester_pays: bool = False,
-                          run_type: str='pre'):
-        datasets_to_merge = []
+                          aws_requester_pays: bool = False):
+        nbr_datasets = []
+        cloud_datasets = []
         memfiles_to_close = []
         self.logger.info(f'    - Processing {len(stac_items)} tiles to create an NBR mosaic')
 
-        for item in stac_items:
-            nbr_array, meta = self.__calculate_nbr_for_item(item=item, perimeter_gdf=perimeter_gdf, aws_requester_pays=aws_requester_pays, target_transform=target_transform, target_crs=target_crs, target_shape=target_shape)
-            if nbr_array is not None and nbr_array.size > 0:
-                memfile = rasterio.io.MemoryFile()
-                memfiles_to_close.append(memfile)
-                with memfile.open(**meta) as dataset:
-                    dataset.write(nbr_array)
-                datasets_to_merge.append(memfile.open())
+        
 
-                del nbr_array
+        for item in stac_items:
+            nbr_array, cloud, meta = self.__calculate_nbr_for_item(item=item, perimeter_gdf=perimeter_gdf, aws_requester_pays=aws_requester_pays, target_transform=target_transform, target_crs=target_crs, target_shape=target_shape)
+            if nbr_array is not None and nbr_array.size > 0:
+                mf_nbr = rasterio.io.MemoryFile()
+                memfiles_to_close.append(mf_nbr)
+                with mf_nbr.open(**meta) as ds:
+                    ds.write(nbr_array)
+                nbr_datasets.append(mf_nbr.open())
+
+                if cloud is not None and cloud.size > 0:
+                    mf_cloud = rasterio.io.MemoryFile()
+                    memfiles_to_close.append(mf_cloud)
+                    with mf_cloud.open(**meta) as ds:
+                        ds.write(cloud)
+                    cloud_datasets.append(mf_cloud.open())
+
+                del nbr_array, cloud
                 gc.collect()
             else:
                 self.logger.info(f'    - Skipping empty or invalid NBR result for item {item.id}')
             
 
-        if not datasets_to_merge:
+        if not nbr_datasets:
             self.logger.warning('    - No valid datasets could be processed for the NBR mosaic')
             return None, None, None
 
-        self.logger.info(f'    - Merging {len(datasets_to_merge)} processed tiles into a single NBR mosaic')
+        self.logger.info(f'    - Merging {len(nbr_datasets)} processed tiles into a single NBR mosaic')
         try:
-            mosaic, out_trans = merge.merge(datasets_to_merge)
-            out_meta = datasets_to_merge[0].meta.copy()
-            out_meta.update({'height': mosaic.shape[1], 'width': mosaic.shape[2], 'transform': out_trans, 'crs': target_crs})
+            mosaic_nbr, out_trans = merge.merge(nbr_datasets)
+            mosaic_cloud, _ = merge.merge(cloud_datasets)
+
+            out_meta = nbr_datasets[0].meta.copy()
+            out_meta.update({'height': mosaic_nbr.shape[1], 'width': mosaic_nbr.shape[2], 'transform': out_trans, 'crs': target_crs})
         except Exception as e:
             self.logger.error(f'    - Error during rasterio.merge for NBR mosaic: {e}')
-            return None, None, None
+            return None, None, None, None, None
         finally:
-            for ds in datasets_to_merge:
+            all_datasets = nbr_datasets + cloud_datasets
+            for ds in all_datasets:
                 ds.close()
             for mf in memfiles_to_close:
                 mf.close()
         self.logger.info('    - NBR Mosaic created successfully')
-        return mosaic, out_meta, out_trans
+        return mosaic_nbr, mosaic_cloud, out_meta, out_trans
 
 
     def create_rgb_mosaic(self, stac_items: list,
@@ -215,8 +229,7 @@ class STAC:
                           target_transform=None, 
                           target_crs=None, 
                           target_shape=None,
-                          aws_requester_pays: bool = False,
-                          run_type: str='pre'):
+                          aws_requester_pays: bool = False):
         datasets_to_merge = []
         memfiles_to_close = []
         self.logger.info(f'    - Processing {len(stac_items)} tiles to create an RGB mosaic')
@@ -255,14 +268,28 @@ class STAC:
         self.logger.info('    - RGB Mosaic created successfully')
         return mosaic, out_meta, out_trans
 
+    def rasterize_water(self, water: gpd.geodataframe, out_height, out_width, out_transform):
+        if not water.empty:
+            water_geoms = [geom for geom in water.geometry]
+            is_water = rasterize(
+                shapes=water_geoms,
+                out_shape=(out_height, out_width),
+                transform=out_transform,
+                fill=0,
+                default_value=1,
+                dtype=np.uint8
+            ) == 1
+        else:
+            is_water = np.zeros((out_height, out_width), dtype=bool)
+        return is_water
+
 
     def __calculate_nbr_for_item(self, item: 'pystac.Item', 
                                perimeter_gdf: gpd.GeoDataFrame, 
                                target_transform=None, 
                                target_crs=None, 
                                target_shape=None,
-                               aws_requester_pays: bool = False,
-                               run_type: str='pre'):
+                               aws_requester_pays: bool = False):
         """
         Calculates Normalized Burn Ratio (NBR) for a given STAC item, clipped to the perimeter.
         Aligns to target_transform, target_crs, and target_shape if provided.
@@ -404,7 +431,13 @@ class STAC:
         nbr[valid_mask] = numerator[valid_mask] / denominator[valid_mask]
         nbr = np.clip(nbr, -1.0, 1.0)
 
-        del nir_data, swir_data, numerator, denominator, valid_mask
+        is_cloud = self.__extract_masks_for_item(item, out_transform=out_transform, out_height=out_height, out_width=out_width, target_crs=target_crs)
+
+        cloud_band = np.zeros(nbr.shape, dtype=np.float32)
+        cloud_band[is_cloud] = 1.0
+
+
+        del nir_data, swir_data, numerator, denominator, valid_mask, is_cloud
         gc.collect()
 
         clip_geom = [geom.__geo_interface__ for geom in perimeter_gdf_proj.geometry]
@@ -427,6 +460,11 @@ class STAC:
                 temp_dataset.write(nbr, 1) # Write the NBR array to the temporary dataset
                 clipped_nbr_array, clipped_transform = rasterio.mask.mask(temp_dataset, clip_geom, crop=True, nodata=np.nan)
 
+        with rasterio.io.MemoryFile() as memfile:
+            with memfile.open(**temp_meta) as temp_dataset:
+                temp_dataset.write(cloud_band, 1) # Write the NBR array to the temporary dataset
+                clipped_cloud, clipped_transform = rasterio.mask.mask(temp_dataset, clip_geom, crop=True, nodata=np.nan)
+
         # Update metadata for the final clipped output
         meta = temp_meta.copy()
         meta.update({
@@ -436,7 +474,7 @@ class STAC:
             "crs": target_crs # Ensure CRS is explicitly set here as well
         })
 
-        return clipped_nbr_array, meta
+        return clipped_nbr_array, clipped_cloud, meta
     
 
     def __calculate_rgb_for_item(self, item: 'pystac.Item', 
@@ -527,6 +565,45 @@ class STAC:
         except Exception as e:
             self.logger.error(f'Error: Failed to reproject tile {item.id}. Skipping. Error: {e}')
             return None, None
+
+    def __extract_masks_for_item(self, item: 'pystac.Item', out_transform, out_height, out_width, target_crs):
+        is_cloud = np.zeros((out_height, out_width), dtype=bool)
+
+        qa_key = next((k for k in ['scl', 'SCL', 'qa_pixel', 'QA_PIXEL'] if k in item.assets), None)
+        if not qa_key:
+            return is_cloud
+
+        try:
+            with rasterio.open(item.assets[qa_key].href) as src_qa:
+                qa_reprojected = np.empty((1, out_height, out_width), dtype=np.uint16)
+                reproject(
+                    source=rasterio.band(src_qa, 1),
+                    destination=qa_reprojected,
+                    src_transform=src_qa.transform,
+                    src_crs=src_qa.crs,
+                    dst_transform=out_transform,
+                    dst_crs=target_crs,
+                    resampling=Resampling.nearest,
+                    src_nodata=src_qa.nodata,
+                    dst_nodata=0
+                )
+                qa_data = qa_reprojected[0]
+
+                if qa_key.lower() == 'scl':
+                    # Sentinel-2 cloud values
+                    # 3: Cloud Shadow, 8: Cloud Medium Probability, 9: Cloud High Probability, 10: Thin Cirrus
+                    is_cloud = np.isin(qa_data, [3, 8, 9, 10])
+
+                else:
+                    # Landsat bitmask checks
+                    # 2: Dilated Cloud, 3: Cloud, 4: Cloud Shadow
+                    is_cloud = ((qa_data & (1 << 1)) != 0) | ((qa_data & (1 << 2)) != 0) | ((qa_data & (1 << 3)) != 0) | ((qa_data & (1 << 4)) != 0)
+        
+        except Exception as e:
+            self.logger.warning(f' Could not process qa mask for item {item.id}: {e}')
+
+        return is_cloud
+
 
     @staticmethod
     def resample_raster_to_match(source_path, ref_transform, ref_crs, ref_width, ref_height) -> MemoryFile:
