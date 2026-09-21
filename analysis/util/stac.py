ae02eaf7402709868302e3b15ef5707fa88bd7ed
GraydonShevchenko <Graydon.Shevchenko@gov.bc.ca>
2026-09-21 11:17:55 -0700

    Refactored stac.py to be more efficient with memory

--- analysis/util/stac.py
+++ analysis/util/stac.py
@@ -5,6 +5,7 @@ from rasterio.io import MemoryFile
 from rasterio.transform import array_bounds
 from rasterio import mask
 from rasterio.windows import from_bounds
+from rasterio.features import geometry_mask
 import geopandas as gpd
 import numpy as np
 import rasterio
@@ -199,7 +200,6 @@ class STAC:
                         ds.write(mask)
                     mask_filepaths.append(path_mask)
 
-
                 del nbr_array, mask
                 gc.collect()
             else:
@@ -213,24 +213,47 @@ class STAC:
         self.logger.info(f'    - Merging {len(nbr_filepaths)} processed tiles into a single NBR mosaic')
 
         datasets_to_merge = [rasterio.open(path) for path in nbr_filepaths]
-        mask_datasets_to_merge = [rasterio.open(path) for path in mask_filepaths]
-
         try:
             mosaic_nbr, out_trans = merge.merge(datasets_to_merge)
-            mosaic_mask, _ = merge.merge(mask_datasets_to_merge)
-
             out_meta = datasets_to_merge[0].meta.copy()
-            out_meta.update({'height': mosaic_nbr.shape[1], 'width': mosaic_nbr.shape[2], 'transform': out_trans, 'crs': target_crs})
         except Exception as e:
             self.logger.error(f'    - Error during rasterio.merge for NBR mosaic: {e}')
             return None, None, None, None
         finally:
-            for ds in datasets_to_merge + mask_datasets_to_merge:
+            for ds in datasets_to_merge:
                 ds.close()
 
-            for path in nbr_filepaths + mask_filepaths:
-                if os.path.exists(path):
-                    os.remove(path)
+        gc.collect()
+
+        mask_datasets_to_merge = [rasterio.open(path) for path in mask_filepaths]
+        try:
+            mosaic_mask, _ = merge.merge(mask_datasets_to_merge)
+        except Exception as e:
+            self.logger.error(f'    - Error during rasterio.merge for NBR mosaic: {e}')
+            return None, None, None, None
+        finally:
+            for ds in mask_datasets_to_merge:
+                ds.close()
+
+        for path in nbr_filepaths + mask_filepaths:
+            if os.path.exists(path):
+                os.remove(path)
+
+        gc.collect()
+        # try:
+        #     mosaic_nbr, out_trans = merge.merge(datasets_to_merge)
+        #     mosaic_mask, _ = merge.merge(mask_datasets_to_merge)
+
+        #     out_meta = datasets_to_merge[0].meta.copy()
+        #     out_meta.update({'height': mosaic_nbr.shape[1], 'width': mosaic_nbr.shape[2], 'transform': out_trans, 'crs': target_crs})
+        # except Exception as e:
+        #     self.logger.error(f'    - Error during rasterio.merge for NBR mosaic: {e}')
+        #     return None, None, None, None
+        # finally:
+        #     for ds in datasets_to_merge + mask_datasets_to_merge:
+        #         ds.close()
+
+            
 
         self.logger.info('    - NBR Mosaic created successfully')
         return mosaic_nbr, mosaic_mask, out_meta, out_trans
@@ -405,7 +428,10 @@ class STAC:
                     src_nodata=nodata_val_nir,
                     dst_nodata=np.nan
                 )
-                nir_data = nir_reprojected[0]
+                nir_data = nir_reprojected[0].copy()
+                del nir_reprojected
+                gc.collect()
+
                 nodata_mask_nir = (nir_data == nodata_val_nir) | (nir_data == 0)
                 nir_data = self.process_reflectance(data=nir_data, band='NIR', sensor=sensor)
                 nir_data[nodata_mask_nir] = np.nan
@@ -424,7 +450,10 @@ class STAC:
                     dst_nodata=np.nan
                 )
 
-                swir_data = swir_reprojected[0]
+                swir_data = swir_reprojected[0].copy()
+                del swir_reprojected
+                gc.collect()
+
                 nodata_mask_swir = np.isnan(swir_data) | (swir_data == 0)
                 swir_data = self.process_reflectance(data=swir_data, band='SWIR2', sensor=sensor)
                 swir_data[nodata_mask_swir] = np.nan
@@ -432,24 +461,33 @@ class STAC:
             except Exception as e:
                 self.logger.error(f'Error reading/reprojecting COG data for item {item.id}: {e}\n Traceback: {traceback.print_exc()}')
                 return None, None, None
-
+        
         try:
             self.logger.info('Calculating nbr')
             # Calculate NBR on the reprojected and aligned data
-            numerator = nir_data - swir_data
+            # numerator = nir_data - swir_data
+            # denominator = nir_data + swir_data
+            # nbr = np.full(nir_data.shape, np.nan, dtype=np.float32)
+            # valid_mask = (denominator != 0) & ~np.isnan(denominator) & ~np.isnan(numerator)
+            # nbr[valid_mask] = numerator[valid_mask] / denominator[valid_mask]
+            # nbr = np.clip(nbr, -1.0, 1.0)
+
             denominator = nir_data + swir_data
+            valid_mask = (denominator != 0) & ~np.isnan(denominator) & ~np.isnan(nir_data) & ~np.isnan(swir_data)
             nbr = np.full(nir_data.shape, np.nan, dtype=np.float32)
-            valid_mask = (denominator != 0) & ~np.isnan(denominator) & ~np.isnan(numerator)
-            nbr[valid_mask] = numerator[valid_mask] / denominator[valid_mask]
+            nbr[valid_mask] = (nir_data[valid_mask] - swir_data[valid_mask]) / denominator[valid_mask]
             nbr = np.clip(nbr, -1.0, 1.0)
+
+            del denominator, valid_mask
+            gc.collect()
 
             self.logger.info('Getting masks')
             is_cloud, is_shadow, is_snow = self.__extract_masks_for_item(item, out_transform=out_transform, out_height=out_height, out_width=out_width, target_crs=target_crs, nir_data=nir_data)
 
-            all_masks = np.zeros(nbr.shape, dtype=np.float32)
-            all_masks[is_cloud | is_shadow | is_snow] = 1.0
+            all_masks = np.zeros(nbr.shape, dtype=np.uint8)
+            all_masks[is_cloud | is_shadow | is_snow] = 1
 
-            del nir_data, swir_data, numerator, denominator, valid_mask, is_cloud, is_shadow, is_snow
+            del nir_data, swir_data, is_cloud, is_shadow, is_snow
             gc.collect()
 
             self.logger.info('Gathering clip geometry')
@@ -469,17 +507,24 @@ class STAC:
                 "nodata": np.nan
             }
 
-            self.logger.info('Clipping nbr')
-            with rasterio.io.MemoryFile() as memfile:
-                with memfile.open(**temp_meta) as temp_dataset:
-                    temp_dataset.write(nbr, 1) # Write the NBR array to the temporary dataset
-                    clipped_nbr_array, clipped_transform = rasterio.mask.mask(temp_dataset, clip_geom, crop=True, nodata=np.nan)
+            self.logger.info('Clipping nbr and mask')
+            minx, miny, maxx, maxy = perimeter_gdf_proj.total_bounds
+            crop_window = rasterio.windows.from_bounds(minx, miny, maxx, maxy, transform=out_transform).round_offsets().round_lengths()
 
-            self.logger.info('Clipping mask')
-            with rasterio.io.MemoryFile() as memfile:
-                with memfile.open(**temp_meta) as temp_dataset:
-                    temp_dataset.write(all_masks, 1) # Write the cloud array to the temporary dataset
-                    clipped_mask, clipped_transform = rasterio.mask.mask(temp_dataset, clip_geom, crop=True, nodata=np.nan)
+            col_off, row_off = int(crop_window.col_off), int(crop_window.row_off)
+            width, height = int(crop_window.width), int(crop_window.height)
+
+            clipped_transform = rasterio.windows.transform(crop_window, out_transform)
+
+            clipped_nbr_array = nbr[row_off:row_off + height, col_off:col_off + width]
+            clipped_mask = all_masks[row_off:row_off + height, col_off:col_off + width]
+
+            geom_mask = geometry_mask(clip_geom, out_shape=(height, width), transform=clipped_transform, invert=True)
+            clipped_nbr_array[~geom_mask] = np.nan
+            clipped_mask[~geom_mask] = 0
+
+            clipped_nbr_array = np.expand_dims(clipped_nbr_array, axis=0)
+            clipped_mask = np.expand_dims(clipped_mask, axis=0)
 
             # Update metadata for the final clipped output
             meta = temp_meta.copy()
@@ -541,10 +586,15 @@ class STAC:
                     src_window = src_window.round_offsets().round_lengths()
                     window_transform = src_meta_check.window_transform(src_window)
 
-                for band_name in ['red', 'green', 'blue']:
+                # for band_name in ['red', 'green', 'blue']:
+                #     with rasterio.open(assets[band_name]) as src:
+                #         band_subset = src.read(1, window=src_window).astype(np.uint16)
+                #         rgb_bands_data.append(band_subset)
+
+                rgb_array = np.empty((3, int(src_window.height), int(src_window.width)), dtype=np.uint16)
+                for idx, band_name in enumerate(['red', 'green', 'blue']):
                     with rasterio.open(assets[band_name]) as src:
-                        band_subset = src.read(1, window=src_window).astype(np.uint16)
-                        rgb_bands_data.append(band_subset)
+                        src.read(1, window=src_window, out=rgb_array[idx])
 
                 if perimeter_gdf.crs is None:
                     perimeter_target_crs = perimeter_gdf.set_crs("EPSG:4326", allow_override=True).to_crs(target_crs)
@@ -560,7 +610,7 @@ class STAC:
                 self.logger.warning(f'Warning: Error reading/clipping RGB COG for item {item.id}. Skipping. Error: {e}')
                 return None, None
 
-        rgb_array = np.stack(rgb_bands_data, axis=0)
+        # rgb_array = np.stack(rgb_bands_data, axis=0)
 
         del rgb_bands_data
         gc.collect()
